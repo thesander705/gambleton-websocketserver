6c1a46702d0b639a2a9900b55fb9c355b1f80884
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-12-30 14:11:13 +0100

    Basic http request and response is set-up

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const fs = require('fs');
+const request = require('request');
+const API_SERVER = 'http://localhost:8080';
 
 // Loading the index file . html displayed to the client
-const server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
+const server = http.createServer(function (req, res) {
+    fs.readFile('./index.html', 'utf-8', function (error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
@@ -15,7 +17,42 @@ const io = require('socket.io').listen(server);
 // When a client connects, we note it in the console
 io.sockets.on('connection', function (socket) {
     console.log('A client is connected!');
+
+    socket.emit("client-connected", "client connected");
+
+
+    socket.on('post-userByAuthToken', function (m) {
+        postRequest("/userByAuthToken", m, socket, 'post-userByAuthToken');
+    });
 });
+
+function postRequest(path, body, socket, event) {
+    request.post(
+        API_SERVER + path,
+        { json: body },
+        function (error, response, body) {
+            if (!error) {
+                socket.emit(event, body);
+            }else{
+                socket.emit(event, error);
+            }
+        }
+    );
+}function
+
+postRequestMessageAllSocketClients(path, body, event) {
+    request.post(
+        API_SERVER + path,
+        { json: body },
+        function (error, response, body) {
+            if (!error) {
+                this.sockets.emit(event, body);
+            }else{
+                this.sockets.emit(event, error);
+            }
+        }
+    );
+}
 
 
 server.listen(3000);
